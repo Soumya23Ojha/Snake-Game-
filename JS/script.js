6f5eccfdcd4e068e55828fc05781e6b0ef2d2b64
soumya ojha <soumyaojha685@gmail.com>
2026-06-10 15:40:01 +0530

    updated the script fir the snake to move and control w the arrow keys

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,13 @@ const board = document.getElementById("game-board");
 
 const GRID_SIZE = 20;
 
-// Create 400 cells
+let snake = [
+    { x: 10, y: 10 },
+    { x: 9, y: 10 },
+    { x: 8, y: 10 }
+];
+let direction = "right";
+
 function createBoard() {
     board.innerHTML = "";
 
@@ -13,4 +19,80 @@ function createBoard() {
     }
 }
 
-createBoard();
+function drawSnake() {
+    const cells = document.querySelectorAll(".cell");
+
+    snake.forEach(segment => {
+        const index = segment.y * GRID_SIZE + segment.x;
+        cells[index].classList.add("snake");
+    });
+}
+
+function moveSnake() {
+
+    const head = { ...snake[0] };
+
+    switch(direction) {
+        case "up":
+            head.y--;
+            break;
+
+        case "down":
+            head.y++;
+            break;
+
+        case "left":
+            head.x--;
+            break;
+
+        case "right":
+            head.x++;
+            break;
+    }
+
+    snake.unshift(head);
+
+    snake.pop();
+
+    render();
+}
+
+document.addEventListener("keydown", (event) => {
+
+    switch(event.key) {
+
+        case "ArrowUp":
+            if(direction !== "down") {
+                direction = "up";
+            }
+            break;
+
+        case "ArrowDown":
+            if(direction !== "up") {
+                direction = "down";
+            }
+            break;
+
+        case "ArrowLeft":
+            if(direction !== "right") {
+                direction = "left";
+            }
+            break;
+
+        case "ArrowRight":
+            if(direction !== "left") {
+                direction = "right";
+            }
+            break;
+    }
+
+});
+
+function render() {
+    createBoard();
+    drawSnake();
+}
+
+render();
+
+setInterval(moveSnake, 300);
